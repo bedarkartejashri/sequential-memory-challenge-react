63361ebb9ad2c0914b86364d95a27fdebaa695eb
tejashri-braincoins <bedarkartejashri@braincoins.me>
2025-07-11 02:02:34 -0400

    Initial Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+// import GameBoard from './components/GameBoard';
+function App() {
+  return (
+    <div class="container">
+      <h1>Sequencial Memory Challenge</h1>
+      {/* <GameBoard /> */}
+    </div>
+  )
+}
+
+export default App
